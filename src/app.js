d6f905f7f588389981d1a279fae0a439a381dfa1
Luroz <luisrpirela@gmail.com>
2021-03-01 00:07:07 -0400

    middleware fix

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
-const userRouter = require('./routers/user')
+const router = require('./routers/user')
 //const bodyParser = require("body-parser")
 const cors = require("cors");
 //const url = require("url");
 const port = process.env.PORT
+const mw = require('./middleware/appMiddleware.js')
 require('./db/db')
 
 const app = express()
 
+
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//app.use(userRouter)
+app.use(mw({ option1: router, option2: router }))
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
