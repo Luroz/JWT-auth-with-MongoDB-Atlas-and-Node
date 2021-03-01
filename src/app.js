8ae56f3410815336d3241d982144d0d751b9af45
Luroz <luisrpirela@gmail.com>
2021-02-28 23:01:36 -0400

    db connection

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const userRouter = require('./routers/user')
+//const bodyParser = require("body-parser")
+const cors = require("cors");
+//const url = require("url");
 const port = process.env.PORT
 require('./db/db')
 
 const app = express()
 
-app.use(express.json())
-app.use(userRouter)
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//app.use(userRouter)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Direccion Inexistente." });
+});
